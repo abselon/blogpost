0c570f4677bba8b1595f5d119df3e68e0e45ccf4
abselon <muhammadahmadessa13@gmail.com>
2023-06-01 18:18:31 +0500

    second commit

--- app/Http/Controllers/examplecontroller.php
+++ app/Http/Controllers/examplecontroller.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class examplecontroller extends Controller
 {
-    //
+    public function homepage() 
+    {
+        return '<h1> Home page </h1> <a href="/about"> go to about page <a>';
+    }
+
+    public function aboutpage()
+    {
+        return '<h1> about page </h1> <a href="/"> Back to home <a>';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\examplecontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return '<h1> Home page </h1> <a href="/about"> go to about page <a>';
-});
+Route::get('/', [examplecontroller::class, "homepage"]);
 
-Route::get('/about', function(){
-    return '<h1> about page </h1> <a href="/"> Back to home <a>';
-}); 
+Route::get('/about', [examplecontroller::class, "aboutpage"]); 
